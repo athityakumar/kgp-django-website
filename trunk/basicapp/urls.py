9cfbff76352da2c11b3d86df0237677fb5325613
snktagarwal <snktagarwal@f52c1ff0-4c2e-11de-b305-25f493754997>
2009-05-29 09:15:55 +0000

    initial structure...

---
+++ trunk/basicapp/urls.py
@@ -0,0 +1,37 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+
+urlpatterns = patterns('django.views.generic.simple',
+    # Example:
+    # (r'^basicapp/', include('basicapp.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+	(r'^blog/', include('basicapp.basic.blog.urls')),
+	(r'^admin/(.*)', admin.site.root),	
+	(r'^comments/', include('basicapp.basic.comments.urls')),	
+	(r'^downloads/',include('basicapp.basic.downloads.urls')),
+	(r'^forum/', include('basicapp.forum.urls')),	
+	(r'^account/', include('django_authopenid.urls')),
+	(r'^$','redirect_to', {'url': '/account/signin/complete'}),
+	(r'^cab/',include('basicapp.cab.urls')),
+	(r'^videos/',include('basicapp.videos.urls')),
+	(r'^accounts/login/','redirect_to',{'url':'/account/signin/complete'}),
+	(r'^wiki/',include('basicapp.wiki.urls')),
+)
+
+
+
+
+
+
+
+
