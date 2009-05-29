5fa5dafcc6510560451957aff8a993834f58a57e
snktagarwal <snktagarwal@f52c1ff0-4c2e-11de-b305-25f493754997>
2009-05-29 10:14:01 +0000

    fancy code highlighter

---
+++ basicapp/cab/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from cab.models import Language, Tag, Snippet, Rating, Bookmark
+
+admin.site.register(Language)
+admin.site.register(Tag)
+admin.site.register(Snippet)
+admin.site.register(Rating)
+admin.site.register(Bookmark)
